d5c2c5e2ca104710bb29fc2f498419c5ccc1cafd
Yuri Katayama <yurikbcarvalho@gmail.com>
2019-11-28 15:53:38 -0300

    First Commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	float vida = 100;
+	bool isAlive = true;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+		ChecarSeEstaVivo();
+		Debug.Log("Esta Vivo: " + ChecarSeEstaVivo());
+	}
+
+	public bool ChecarSeEstaVivo () {
+
+		// isAlive = (vida <= 0) ? false : true;
+		// if (vida <= 0) isAlive = false;
+
+		if (vida <= 0) {
+			isAlive = false;
+		} else {
+			isAlive = true;
+		}
+		return isAlive;
+	}
+}
